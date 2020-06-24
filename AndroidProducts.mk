36065472f49b34d1060092ec9f0f3649674ae681
EdwardWu <56945729+bluehomewu@users.noreply.github.com>
2020-06-24 21:01:24 +0800

    Z01R : Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_Z01R.mk
+    $(LOCAL_DIR)/havoc_Z01R.mk
 	
 COMMON_LUNCH_CHOICES := \
-    aosp_Z01R-user \
-    aosp_Z01R-userdebug \
-    aosp_Z01R-eng 
+    havoc_Z01R-user \
+    havoc_Z01R-userdebug \
+    havoc_Z01R-eng 
